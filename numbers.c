932ab3c575c741514ae1a946897809f81a491ba3
sharontinuk <112137670+sharontinuk@users.noreply.github.com>
2022-10-18 20:01:39 +0100

    Update numbers.c

--- numbers.c
+++ numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isdigit - checks if character is digi
+ * _isdigit - checks if character is digit
  * @c: the character to check
  *
  * Return: 1 if digit, 0 otherwise
